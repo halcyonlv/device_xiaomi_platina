7d66cb4f31f814aed46564b69b176df68ce56f77
Michael Bestas <mkbestas@lineageos.org>
2026-06-11 10:00:02 +0300

    platina: Switch to py extract utils
    Signed-off-by: HeroBuxx <me@herobuxx.me>
    Signed-off-by: Raya Mahardika <94042378+rktdnt@users.noreply.github.com>

---
+++ extract-files.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_17_2'
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib/libMiCameraHal.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'platina',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sdm660-common', module.vendor)
+    utils.run()
