8b579af9b80ae48f0770f74218f329f5dd9625f3
HeroBuxx <me@herobuxx.me>
2026-08-06 01:03:38 +0000

    platina: Enchanting shiny platinum
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_platina.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_platina-user \
-    lineage_platina-userdebug \
-    lineage_platina-eng
+    $(LOCAL_DIR)/halcyon_platina.mk
